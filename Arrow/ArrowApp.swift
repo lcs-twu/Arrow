790352694b555284cf35a020ca96acc3495178fc
Tom Wu <twu@lcs.on.ca>
2023-10-26 14:09:14 -0400

    Initial Commit

---
+++ Arrow/ArrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArrowApp.swift
+//  Arrow
+//
+//  Created by Tom Wu on 2023-10-26.
+//
+
+import SwiftUI
+
+@main
+struct ArrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
